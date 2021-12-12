4ac0f041e7cf4adfb79c4a408c9bd92fc5552df7
赖咸旭 <laixianxu@sina.com>
2021-12-12 14:42:00 +0800

    vue3-ewshop build

---
+++ src/views/Home.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+      {{banner}}
+  </div>
+</template>
+
+<script>
+import {ref, onMounted} from 'vue';
+import {getHomeAllData, getBanner} from "network/home";
+
+export default {
+  name: 'Home',
+
+  setup(){
+      const banner = ref([]);
+
+      onMounted(()=>{
+        getHomeAllData().then(res=>{
+
+              banner.value = res.slides;
+
+
+        }).catch(err=>{
+
+        })
+
+      })
+
+      return {
+        banner
+      }
+  },
+
+  components: {
+
+  }
+}
+</script>
+
+<style>
+  #demo {
+    background:url('~assets/images/3.png') no-repeat;
+    width:100px;
+    height:100px;
+  }
+  .demo1 {
+    color:var(--color-high-text)
+  }
+</style>
